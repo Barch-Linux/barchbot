1657670aeb9ca06f7608a3a63429b73ba7045586
BBaoVanC <github@bbaovanc.com>
2021-01-01 01:18:44 -0600

    Load cogs using list of cogs

--- barchbot.py
+++ barchbot.py
@@ -8,14 +8,17 @@ https://github.com/Barch-Linux/barchbot
 
 from discord.ext import commands
 import json
-import os
 
 TOKENS = dict(json.load(open("tokens.json", "r")))
 
 bot = commands.Bot(command_prefix=":b:")
 
-for cog in os.listdir("./cogs"):
-    bot.load_extension("cogs.bname")  # cog to add B to usernames
+cogs = [
+    "bname",
+]  # list of cogs
+
+for cog in cogs:
+    bot.load_extension(f"cogs.{cog}")  # load the cog
 
 
 bot.run(TOKENS['DISCORD_BOT_TOKEN'])
